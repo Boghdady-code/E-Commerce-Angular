44ee6f716d31f8caca50f74e6254e2efbea99931
Boghdady-code <ahmedboghdady18@gmail.com>
2023-11-25 21:54:47 +0200

    responsive editing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,80 +2,55 @@ import { ProductService } from './../../services/product.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Element } from '@angular/compiler';
 
-
-
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit{
-  productsArray:any[]=[];
-  receievedData:any[]=[];
-  loading:boolean=false;
-  isExist:boolean=false;
-  @ViewChild('quantity') inputCountField!:ElementRef;
+export class HomeComponent implements OnInit {
+  productsArray: any[] = [];
+  receievedData: any[] = [];
+  loading: boolean = false;
+  isExist: boolean = false;
+  @ViewChild('quantity') inputCountField!: ElementRef;
 
-  constructor(private productService:ProductService) {
+  constructor(private productService: ProductService) {}
 
+  getProductData() {
+    this.loading = true;
+    this.productService.showProducts().subscribe((response) => {
+      this.loading = false;
+      console.log(response);
+      this.productsArray = response;
+    });
   }
 
-    getProductData () {
-      this.loading=true;
-      this.productService.showProducts().subscribe((response)=>{
-      this.loading=false;
-      console.log (response);
-      this.productsArray=response;
-    })
-
+  saveProduct({ product, quantity }: any): any {
+    this.productService.saveProduct({ product, quantity });
   }
 
-  saveProduct({product, quantity}:any):any {
-    this.productService.saveProduct({product, quantity});
-
-
-  }
-
-
-
-
-
-  getCategory(eventInfo:any){
-
-    if(eventInfo == false) {
+  getCategory(eventInfo: any) {
+    if (eventInfo == false) {
       this.loading;
 
       this.getProductData();
-  } else {
-    this.loading;
+    } else {
+      this.loading;
 
-    this.productsArray=eventInfo;
+      this.productsArray = eventInfo;
+    }
   }
-
-  }
-
-
-
-
-
 
   ngOnInit(): void {
     this.getProductData();
-    this.productService.isProductExist.subscribe(response => {
-          this.isExist=response;
+    this.productService.isProductExist.subscribe((response) => {
+      this.isExist = response;
 
-       setTimeout(() => {
-      this.isExist=false;
+      setTimeout(() => {
+        this.isExist = false;
+      }, 15000);
+    });
 
-    }, 10000);
-
-    })
-
-
-
-
+    console.log(this.isExist);
   }
-
 }
